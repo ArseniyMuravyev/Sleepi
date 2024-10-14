6e21645efc25583609f929eabcec19e6dad55316
Mursey <a@mursey.online>
2024-10-14 07:05:33 +0300

    fix(list components): fix list view on mobile

--- components/additional-reading/ReadingList.tsx
+++ components/additional-reading/ReadingList.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from "next-intl";
 export const ReadingList: FC = () => {
   const t = useTranslations();
   return (
-    <ul className="list-disc flex flex-col gap-4 w-full z-10">
+    <ul className="list-disc flex flex-col gap-4 w-[calc(100%-16px)] z-10 ml-4">
       {reading.map((item) => (
         <li key={item.title}>
           <a href={t(item.link)} className="underline font-bold text-white">
--- components/glossary/WordsList.tsx
+++ components/glossary/WordsList.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 export const WordsList: FC = () => {
   const t = useTranslations();
   return (
-    <ul className="list-disc flex flex-col gap-4 w-full">
+    <ul className="list-disc flex flex-col gap-4 w-[calc(100%-16px)] z-10 ml-4">
       {glossaryWords.map((item) => (
         <li key={item.title}>
           <p>
