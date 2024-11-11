1e8a57df1e8273fae076d373143547ecc71886f4
Mursey <a@mursey.online>
2024-11-11 18:56:37 +0300

    feat(footer.tsx): fix footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,6 +4,6 @@ import { LanguageSwitcher } from "../language-switcher/LanguageSwitcher";
 export const Footer: FC = () => (
   <footer className="flex flex-col gap-8 items-center justify-center py-10">
     <LanguageSwitcher />
-    <p>© Sleepi 2024-2025</p>
+    <p>© Sleepi 2024</p>
   </footer>
 );
